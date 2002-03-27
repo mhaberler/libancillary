4f76dde7fb53af01941965d4896c5a6001d60414
cigaes <cigaes>
2002-03-27 16:12:23 +0000

    *** empty log message ***

--- ancillary.h
+++ ancillary.h
@@ -1,3 +1,33 @@
+/***************************************************************************
+ * libancillary - black magic on Unix domain sockets
+ * (C) Nicolas George
+ * ancillary.c - public header
+ ***************************************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef ANCILLARY_H__
 #define ANCILLARY_H__
 
@@ -33,8 +63,40 @@ extern int ancil_recv_fds_with_buffer(int sock, int *fds, unsigned n_fds, void *
  * Returns: -1 and errno in case of error, 0 in case of success.
  */
 
+#define ANCIL_FD_BUFFER(n) \
+    struct { \
+	struct cmsghdr h; \
+	int fd[n]; \
+    }
+/* ANCIL_FD_BUFFER(n)
+ *
+ * A structure type suitable to be used as buffer for n file descriptors.
+ * Requires <sys/socket.h>.
+ * Example:
+ * ANCIL_FD_BUFFER(42) buffer;
+ * ancil_recv_fds_with_buffer(sock, 42, my_fds, &buffer);
+ */
+
 extern int ancil_send_fds(int sock, const int *fds, unsigned n_fds);
+/*
+ * ancil_send_fds(sock, n_fds, fds)
+ *
+ * Sends the file descriptors in the array pointed by fds, of length n_fds
+ * on the socket sock.
+ * n_fds must not be greater than ANCIL_MAX_N_FDS.
+ * Returns: -1 and errno in case of error, 0 in case of success.
+ */
+
 extern int ancil_recv_fds(int sock, int *fd, unsigned n_fds);
+/*
+ * ancil_recv_fds(sock, n_fds, fds)
+ *
+ * Receives n_fds file descriptors into the array pointed by fds
+ * from the socket sock.
+ * n_fds must not be greater than ANCIL_MAX_N_FDS.
+ * Returns: -1 and errno in case of error, 0 in case of success.
+ */
+
 
 extern int ancil_send_fd(int sock, int fd);
 /* ancil_recv_fd(sock, fd);
@@ -48,20 +110,6 @@ extern int ancil_recv_fd(int sock, int *fd);
  *
  * Receives the file descriptor fd from the socket sock.
  * Returns : -1 and errno in case of error, 0 in case of success.
- */
-
-#define ANCIL_FD_BUFFER(n) \
-    struct { \
-	struct cmsghdr h; \
-	int fd[n]; \
-    }
-/* ANCIL_FD_BUFFER(n)
- *
- * A structure type suitable to be used as buffer for n file descriptors.
- * Requires <sys/socket.h>.
- * Example:
- * ANCIL_FD_BUFFER(42) buffer;
- * ancil_recv_fds_with_buffer(sock, 42, my_fds, &buffer);
  */
 
 #endif /* ANCILLARY_H__ */
