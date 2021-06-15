bc1cbdf06d91e88f8037a4299ed2dddc34b018e7
wozg1688 <85914925+wozg1688@users.noreply.github.com>
2021-06-15 20:20:56 +0800

    Revert "用Fix C++ building"

--- ancillary.h
+++ ancillary.h
@@ -31,10 +31,6 @@
 #ifndef ANCILLARY_H__
 #define ANCILLARY_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /***************************************************************************
  * Start of the readable part.
  ***************************************************************************/
@@ -123,9 +119,5 @@ ancil_recv_fd(int, int *);
  * Receives the file descriptor fd from the socket sock.
  * Returns : -1 and errno in case of error, 0 in case of success.
  */
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* ANCILLARY_H__ */
